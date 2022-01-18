9e3eb5fe857f8be9ac90edf277aac39ce58db3e1
user name <asadullahalgalib783@gmail.com>
2022-01-18 14:23:45 +0600

    header and footer part done

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="card-footer text-muted text-center">
+        All Right copy right reserves @2022.This is all created by AH media
+      </div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,72 @@
+<div class="row">
+    <div class="col-md-10 col-sm-10 d-flex justify-content-between m-auto p-3">
+        <div>
+            <img src="logo/Screenshot 2022-01-17 220026.png" style="width:200px;height:170px;">
+        </div>
+        <div class="p-3">
+            <button class="btn btn-success">Add Money</button>
+        </div>
+    </div>
+</div>
+<div class="row" style="background:black;">
+    <div class="col-md-10 col-sm-5 m-auto">
+        <nav class="navbar navbar-expand-lg">
+            <a href="/" class="btn btn-outline rounded-pill">Home</a>
+            <a href="/userdata" class="btn btn-outline rounded-pill">Amader Shomporke Janun</a>
+            <a href="/product" class="btn btn-outline rounded-pill">Amader Karjokrom</a>
+            <a href="/cashier" class="btn btn-outline rounded-pill">Gallary</a>
+            <a href="" class="btn btn-outline rounded-pill">Video</a>
+            <a href="" class="btn btn-outline rounded-pill">Volantiar Regestration</a>
+            <a href="" class="btn btn-outline rounded-pill">Contact</a>
+        </nav>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="slider_image/81014.jpg" class="d-block w-100"  style="width:600px;height:550px;">
+                </div>
+                <div class="carousel-item">
+                    <img src="slider_image/167531.jpg" class="d-block w-100" style="width:600px;height:550px;" >
+                </div>
+                <div class="carousel-item">
+                    <img src="slider_image/168907.jpg" class="d-block w-100" style="width:600px;height:550px;">
+                </div>
+                <div class="carousel-item">
+                    <img src="slider_image/168908.jpg" class="d-block w-100" style="width:600px;height:550px;">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+            </button>
+        </div>  
+    </div>
+</div>
+
+
+
+
+<style>
+    .navbar{
+        align-items:center;
+    }
+    .btn-outline{
+        color:#008b8b;
+        border:solid 2px;
+        border-color:#008b8b;
+        margin:5px;
+    }
+    .btn-outline:hover{
+        color:white;
+        
+        background-color:#008b8b;
+    }
+</style>
