2544851a384f2522c85a0dc516cea65d615d4ae8
user name <asadullahalgalib783@gmail.com>
2022-08-03 20:52:36 +0600

    filter option

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,48 +2,37 @@
 @section('content')
 
 <div class="bg-light">
-<button class="btn btn-danger" id="show">show</button>
-    <div class="row p-4 q">
-        <div class="col-md-12">
-            <h3>Our organaigation</h3>
-            <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
+<button class="btn btn-danger">show</button>
+    <div class="row p-4">
+        <div class="col-md-3">
+            <div class="card p-4">
+                <h3>Our organaigation</h3>
+                <div class="d-flex about-sidebar">
+                    <input type="checkbox" name="about">
+                    <li>About</li>
+                </div>
+                <div class="d-flex about-sidebar">
+                    <input type="checkbox" name="about">
+                    <li>About</li>
+                </div>
+                <div class="d-flex about-sidebar">
+                    <input type="checkbox" name="about">
+                    <li>About</li>
+                </div>
+                <div class="d-flex about-sidebar">
+                    <input type="checkbox" name="about">
+                    <li>About</li>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card p-4">
+                <h3>Our organaigation</h3>
+                <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
+            </div>
         </div>
         
     </div>
-    <button class="btn btn-danger" id="Planshow">show</button>
-    <div class="row plan">
-        <div class="col-md-12">
-            <h3>Our Plan</h3>
-            <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
-        </div>
-    </div>
-    <button class="btn btn-danger" id="Motoshow">show</button>
-    <div class="row moto">
-        <div class="col-md-12">
-            <h3>Our Moto</h3>
-            <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
-        </div>
-    </div>
-   
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Our Expectation</h3>
-            <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Our Activities</h3>
-            <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Our Achievments</h3>
-            <p>It is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us andIt is a small and growing organaigation that made by us.It is a simple and good comoany for us and.It is a small and growing organaigation that made by us.It is a simple and good comoany for us and </p>
-        </div>
-    </div>
+ 
 </div>
 @endsection
